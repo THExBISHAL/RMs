82ec6050571144c97b7a8dc530d2a0ad8f1ca4b3
Bishal Chakraborty <iambishalchakraborty1304@gmail.com>
2025-09-28 14:33:57 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://rmsbishal.netlify.app/",
     methods: ["GET", "PUT", "POST"],
     credentials: true,
   })
